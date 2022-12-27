f595fe9c1ef528bb6a07f1ca679a2bb2de4c74a9
uvacoder <uva22940coder@gmail.com>
2022-12-27 03:02:57 -0500

    Update repos.ts

--- server/api/repos.ts
+++ server/api/repos.ts
@@ -11,7 +11,7 @@ const hiddenRepos = new Set([
 ])
 
 export default defineEventHandler(async () => {
-  const { repos } = await $fetch('https://ungh.cc/orgs/unjs/repos') as any
+  const { repos } = await $fetch('https://ungh.cc/orgs/woodworker4303/repos') as any
   return repos
     .filter((repo: any) => !hiddenRepos.has(repo.name))
     .sort((a: any, b: any) => b.stars - a.stars)
